3b169b38ff0c5a6c723048cbbdeb1ab35b33463d
Amber Akkermans <r0740967@student.thomasmore.be>
2020-11-03 13:27:11 +0100

    Server port aangepast

---
+++ src/main/java/fact/it/employee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package fact.it.employee.controller;
+
+import fact.it.employee.model.Employee;
+import fact.it.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostConstruct
+    public void FillDatabase(){
+        if (employeeRepository.count() == 0){
+            employeeRepository.save(new Employee("E01", "H01", "Emp", "Loyee", "Barman"));
+        }
+        System.out.println("Aantal werknemers: " + employeeRepository.count());
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAllItems(){
+        return employeeRepository.findAll();
+    }
+}
